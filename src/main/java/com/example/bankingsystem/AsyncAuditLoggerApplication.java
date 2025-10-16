75f446bdb43982bec8c2aeecce7da4bfd5c18f27
Naman Muley <naman.g.muley@gmail.com>
2025-10-16 04:07:56 -0700

    Add src/main/java/com/example/bankingsystem/AsyncAuditLoggerApplication.java

---
+++ src/main/java/com/example/bankingsystem/AsyncAuditLoggerApplication.java
@@ -0,0 +1,11 @@
+package com.example.bankingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncAuditLoggerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncAuditLoggerApplication.class, args);
+    }
+}
